de11d7eb017c13fe583eb89a296e99057b652b99
Dmytro Maziar <dmytrom@elementor.com>
2023-05-29 12:31:12 +0200

    test6

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,7 +4,8 @@ test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/blabla/);
+  await expect(page).toHaveTitle(/Playwright/);
+  // test6
 });
 
 test('get started link', async ({ page }) => {
